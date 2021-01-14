cf8b2985d2675c1a96bac6d6b27ce2835df57e17
Vitor Gonçalves Rodrigues <vitor7goncalves@outlook.com>
2021-01-14 16:00:20 -0300

    continuação da criação da página usando Angular

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
 
 @Component({
     selector: 'app-course-list', 
     templateUrl: './course-list.component.html'
 })
 
-export class CourseListComponent{
+export class CourseListComponent implements OnInit{
+     courses: Course[] = []; 
 
+     ngOnInit(): void{
+         this.courses = [
+         {
+             id: 1,
+             name: 'Angular: Form',
+             imageUlr: '',
+             price: 99.99,
+             code: 'XPS-8796',
+             duration: 120,
+             rating: 4.5,
+             releaseDate: 'November, 2, 2019'
+         },
+         {
+            id: 2,
+            name: 'Angular: HTTP',
+            imageUlr: '',
+            price: 45.99,
+            code: 'LKL-1094',
+            duration: 80,
+            rating: 4,
+            releaseDate: 'November, 4, 2019'
+        }
+        ]
+     }
 }
---
+++ src/app/courses/course.ts
@@ -0,0 +1,10 @@
+export class Course{
+    id: number;
+    name: string;
+    imageUlr: string;
+    price: number;
+    code: string;
+    duration: number;
+    rating: number;
+    releaseDate: string;
+}
