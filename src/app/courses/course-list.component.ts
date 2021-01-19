3c68afaa48947c00a186de04e3c27675d4a74d65
Vitor Gonçalves Rodrigues <vitor7goncalves@outlook.com>
2021-01-18 23:08:13 -0300

    Continuando o projeto....

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -8,11 +8,25 @@ import { CourseService } from './course.service';
 })
 
 export class CourseListComponent implements OnInit{
-     courses: Course[] = []; 
+
+    filteredCourses: Course[] = [];
+
+     _courses: Course[] = []; 
+
+     _filter: string;
 
      constructor(private courseSService: CourseService) {}
 
      ngOnInit(): void{ 
-      this.courses = this.courseSService.retrieveAll()
+      this._courses = this.courseSService.retrieveAll()
+      this.filteredCourses = this._courses;
+     }
+     set filter(value: string){
+        this._filter = value;
+
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filter.toLocaleLowerCase()) > -1); 
+     }
+     get filter(){
+         return this._filter;
      }
 }
